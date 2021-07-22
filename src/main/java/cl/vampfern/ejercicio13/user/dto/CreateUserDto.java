991144e1283a2e0bb8ee4fcb8f81c9cc6fff0295
Victor Ampuero <victor.amp.fern@gmail.com>
2021-07-21 23:55:35 -0400

    regex pass

--- src/main/java/cl/vampfern/ejercicio13/user/dto/CreateUserDto.java
+++ src/main/java/cl/vampfern/ejercicio13/user/dto/CreateUserDto.java
@@ -5,6 +5,7 @@ import lombok.Data;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 import java.util.Set;
 
 @Data
@@ -18,6 +19,7 @@ public class CreateUserDto {
     private String email;
 
     @NotEmpty
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{3,}$")
     private String password;
 
     private Set<CreateUserPhoneDto> phones;
